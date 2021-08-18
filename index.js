e2f43b8761b9780c33716dc704f83feeeadacb78
Diego Rojas <diegorojas431@gmail.com>
2021-08-17 21:27:17 -0500

    Variables: var VS let

---
+++ index.js
@@ -0,0 +1,23 @@
+// ------------------------------var
+
+// var a = 'Hola mundo';
+// console.log(a);
+
+// if (true) {
+// 	var a = 3
+// 	console.log(a);
+// }
+
+// console.log(a);
+
+// ------------------------------let
+
+let a = 'Hola mundo';
+console.log(a);
+
+if (true) {
+	let a = 3
+	console.log(a);
+}
+
+console.log(a);
