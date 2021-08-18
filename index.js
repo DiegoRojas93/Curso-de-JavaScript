567bebc5d16b65801338de42ecf656b1b45d8ede
Diego Rojas <diegorojas431@gmail.com>
2021-08-17 22:03:55 -0500

    Cadenas de Texto (Strings)

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-// ------------------------------primitivos
+// -------------------------Cadenas de Texto (Strings)
 
-// const PERRITO = 'Losbeint';
-// console.log(PERRITO);
+let nombre = 'Diego'
+let apellido = "Rojas"
 
-// PERRITO = 'Jaime'
+let saludo =  new String('Hola Mundo!')
 
-// console.log(PERRITO);
+console.log(nombre, apellido, saludo);
 
-// ------------------------------compuestos
+// propiedad
 
-const COLORES= ['Amarillo', 'Azul'];
-console.log('Colores: ', COLORES);
+console.log(nombre.length, apellido.length, saludo.length);
 
-COLORES.push('Rojo')
-console.log('Colores: ', COLORES);
+// metodos
+
+let lorem = 'Eu pariatur sint deserunt laborum quis sint enim occaecat. Ipsum dolore dolor sunt duis tempor Lorem labore tempor veniam laborum officia exercitation labore. Esse do mollit est occaecat irure esse pariatur anim ad quis tempor ipsum consectetur exercitation. In velit deserunt laborum esse sint in tempor. Ea mollit labore quis ipsum esse ea sint labore adipisicing adipisicing minim est labore commodo. Ad et irure in dolor pariatur adipisicing deserunt in.'
+
+console.log(nombre.toUpperCase());
+console.log(apellido.toLowerCase());
+console.log(lorem.includes('amet'));
+console.log(lorem.trim());
+console.log(lorem.split(" "));
