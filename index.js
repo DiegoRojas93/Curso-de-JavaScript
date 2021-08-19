2554db3ff97ae68889b5df47e1da902db8ecb70f
Diego Rojas <diegorojas431@gmail.com>
2021-08-18 20:39:16 -0500

    Funciones

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-// undefined
+// Funcion Declarada
 
-let indefinida;
+function estoEsUnaFuncion(){
+	console.log('uno');
+	console.log('dos');
+	console.log('tres');
+}
 
-console.log('indefinida: ', indefinida);
+// invocacion de funcion
 
-// null
+estoEsUnaFuncion();
 
-let nulo = null;
+// retorno de valores
 
-console.log('null: ', nulo);
+function unaFuncionQueDevuelveUnValor(){
+	return 'La funcion ha retornado una cadena de texto'
+}
 
-// NaN - Not a Number
+let str = unaFuncionQueDevuelveUnValor()
 
-let noEsUnNumero = 'hola' * 3.7;
+console.log(str);
 
-console.log('No es un número: ', noEsUnNumero);
+// parametros
+
+function saludar ( nombre = 'Diego', edad = 27 ){
+	console.log(`Hola mmi nombre es ${nombre} y tengo ${edad} años`);
+}
+
+saludar()
+saludar('Maria', 61);
+
+// funciones declarativa
+
+funcionDeclarada();
+
+function funcionDeclarada(){
+	console.log(`Esto es una función declarada, puede invocarse en cualquer parte de nuestro código, incluso antes de que la funcion sea declarada`);
+};
+
+console.log('');
+
+funcionDeclarada();
+
+// funcion anónima
+
+// funcionAnonima()
+
+const funcionAnonima = function (){
+	console.log('Esto es una funcion expresada, es decir que es una funcion que se le ha asignado como un valor a una variable, si invocamos esta funcion antes de su definicion, JavaScript nos dirá Cannot access "funcionExpresada" before initialization');
+};
+
+funcionAnonima();
