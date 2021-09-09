e2938cb991a87a5500ec66f921266b8bc4189215
Diego Rojas <diegorojas431@gmail.com>
2021-09-08 21:24:10 -0500

    Objeto Cortocircuito

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-/*------------------------- Objeto Math ------------------------*/
+/*------------------ Operador de Cortocircuito -----------------*/
 
-// console.log(Math);
+// Cortocircuito OR
 
-// Métodos
+console.groupCollapsed(`Cortocircuito OR`)
 
-console.log(Math.abs(-7.8));        // valor absoluto
+	console.log(`Cadena` || `Valor de la cadena`)
+	console.log( -19 || `Valor de la cadena`)
+	console.log( true || `Valor de la cadena`)
+	console.log( [] || `Valor de la cadena`)
+	console.log( {} || `Valor de la cadena`)
 
-console.group('redondeo');
+	console.log('');
 
-	console.log(Math.ceil(7.2));      // redondeo hacia arriba
-	console.log(Math.floor(7.8));     // redondeo hacia abajo
-	console.log(Math.round(7.5));     // redondea hacia el numero entero mas cercano
-console.groupEnd('redondeo');
+	console.log( '' || `Valor de la cadena`)
+	console.log( -0 || `Valor de la cadena`)
+	console.log( false || `Valor de la cadena`)
+	console.log( null || `Valor de la cadena`)
+	console.log( undefined || `Valor de la cadena`)
 
-console.log(Math.sqrt(81));         // Raiz cuadrada de 81 = 9
+console.groupEnd(`Cortocircuito OR`)
 
-console.log(Math.pow(2,5));         // Exponenciacion (Base, exponente)
+// Cortocircuito AND
 
-console.log(Math.sign(-7.8));
-// Indica el signo del numero a evaliar: (-1 == negativo), (0 == 0), ( 1 == positivo )
+console.groupCollapsed(`Cortocircuito AND`)
 
+	console.log( '' && `Valor de la cadena`)
+	console.log( -0 && `Valor de la cadena`)
+	console.log( false && `Valor de la cadena`)
+	console.log( null && `Valor de la cadena`)
+	console.log( undefined && `Valor de la cadena`)
 
-console.group('Aleatorio');
+	console.log('');
 
-	console.log(Math.random());
-	// lanza un numero flotante aleatorio comprendido entre 0 a 1
+	console.log(`Cadena` && `Valor de la cadena`)
+	console.log( -19 && `Valor de la cadena`)
+	console.log( true && `Valor de la cadena`)
+	console.log( [] && `Valor de la cadena`)
+	console.log( {} && `Valor de la cadena`)
 
-	console.log(Math.random() * 1000);
-	// lanza un numero flotante aleatorio comprendido entre 0 a 1000
-
-	console.log(Math.round(Math.random() * 1000));
-	// lanza un numero aleatorio comprendido entre 0 a 1000
-
-	console.log(Math.random() * (1000 - 200) + 200 );
-	/* lanza un numero flotante aleatorio comprendido entre el rango de (1000 - 200) y luego le sumas 200 para que este por encima del minimo de 200*/
-
-	console.log(Math.round(Math.random() * (1000 - 200) + 200));
-	/* lanza un numero aleatorio comprendido entre el rango de (1000 - 200) y luego le sumas 200 para que este por encima del minimo de 200*/
-
-console.groupEnd('Aleatorio');
+console.groupEnd(`Cortocircuito AND`)
