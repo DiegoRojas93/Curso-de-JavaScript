57338b4e5d1891deea22dcba758c927a2cc816e9
Diego Rojas <diegorojas431@gmail.com>
2021-09-08 21:32:13 -0500

    alert, confirm y prompt

--- index.js
+++ index.js
@@ -1,41 +1,9 @@
-/*------------------ Operador de Cortocircuito -----------------*/
+/*------------------- alert, confirm y prompt ------------------*/
 
-// Cortocircuito OR
+let alerta = window.alert('Hola Mundo!, soy una alerta')
+    confirmacion = window.confirm('Hola Mundo!, ¿soy una afirmacion?')
+    aviso = window.prompt('Hola Mundo!')
 
-console.groupCollapsed(`Cortocircuito OR`)
-
-	console.log(`Cadena` || `Valor de la cadena`)
-	console.log( -19 || `Valor de la cadena`)
-	console.log( true || `Valor de la cadena`)
-	console.log( [] || `Valor de la cadena`)
-	console.log( {} || `Valor de la cadena`)
-
-	console.log('');
-
-	console.log( '' || `Valor de la cadena`)
-	console.log( -0 || `Valor de la cadena`)
-	console.log( false || `Valor de la cadena`)
-	console.log( null || `Valor de la cadena`)
-	console.log( undefined || `Valor de la cadena`)
-
-console.groupEnd(`Cortocircuito OR`)
-
-// Cortocircuito AND
-
-console.groupCollapsed(`Cortocircuito AND`)
-
-	console.log( '' && `Valor de la cadena`)
-	console.log( -0 && `Valor de la cadena`)
-	console.log( false && `Valor de la cadena`)
-	console.log( null && `Valor de la cadena`)
-	console.log( undefined && `Valor de la cadena`)
-
-	console.log('');
-
-	console.log(`Cadena` && `Valor de la cadena`)
-	console.log( -19 && `Valor de la cadena`)
-	console.log( true && `Valor de la cadena`)
-	console.log( [] && `Valor de la cadena`)
-	console.log( {} && `Valor de la cadena`)
-
-console.groupEnd(`Cortocircuito AND`)
+console.log(alerta);
+console.log(confirmacion);
+console.log(aviso);
