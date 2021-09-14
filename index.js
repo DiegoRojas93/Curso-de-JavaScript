eadc695c241be6950c543c2534ef5fa5e0e41ae9
Diego Rojas <diegorojas431@gmail.com>
2021-09-13 19:27:04 -0500

    Funciones Anónimas Autoejecutables

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-/*------------------- Expresiones regulares ------------------*/
+/*---------------- Funcion anonima autoejecutable --------------*/
 
-const CADENA = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, lorem remaining jhonMircha49@gmail.com essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+(function (){
+	console.log(`Mi primera funcion anonima autoejecutada`);
+})();
+
+// con parametros
+
+(function (d, w, c){
+	c.log(`Mi segunda funcion anonima autoejecutada`);
+
+	c.log('');
+
+	c.log(`Document: `, d);
+	c.log(`window: `, w);
+	c.log(`console: `, c);
+})(document, window, console);
 
 
-let regex = new RegExp('loremfds', 'ig') // new RegExp('expresion', 'Flags')
-console.log(regex.test(CADENA)); // devuelve un true so la expresion existe
-console.log(regex.exec(CADENA)); // devuelve un arreglo de la informacion de la expresion regular
+// Otras for mas de escribir una funcion anonima auto ejecutadas
 
-let regex = /lorem/ig;            // /expresion/flags
-console.log(regex.test(CADENA));  // devuelve un true so la expresion existe
-console.log(regex.exec(CADENA));  // devuelve un arreglo de la informacion de la expresion regular
+// Clásica
 
+(function(){
+	console.log('Version clásica');
+})();
+
+
+// Funcion Crockford
+
+((function(){
+	console.log('Version Crokford');
+})());
+
+
+// Funcion Unaria
+
++function(){
+	console.log('Version clásica');
+}();
+
+// Funcion Facebook
+
+!function(){
+	console.log('Version Facebook');
+}();
