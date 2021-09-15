4037f2e4d48ceb14f4a3e0d9370d779f6078a18f
Diego Rojas <diegorojas431@gmail.com>
2021-09-14 20:18:13 -0500

    Ejercicio 4

--- js/index.js
+++ js/index.js
@@ -1,86 +1,108 @@
 /*------------------------- Ejercicio 2 ------------------------*/
 
-// 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+// 12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
 
-const numeroAleatorio = (min = '', max = '') => {
-	if(!min || typeof min === 'string'){
-		console.warn(`No ingresaste el valor minimo del rango`);
-		return
-	};
-	if(!max || typeof max === 'string') {
-		console.warn(`No ingresaste el valor maximo del rango`);
-		return
+const primo = (num = undefined) => {
+	if (num === undefined) return console.warn(`No ingresaste un numero.`);
+
+	if (typeof num !== 'number') return console.error(`El valor ${num} ingresado no es un numero.`);
+
+	if (num === 0) return console.error(`El cero no es divisible por el mismo, la división entre cero no está definida, por ellos el cero no es un primo.`);
+
+	if (num === 1) return console.error(`El numero 1, por convenio, no es considerado numero primero.`);
+
+	if(Math.sign(num) === -1) return console.error(`EL número no pusde ser negativo`);
+
+	let divisible = false;
+
+	for (let i = 2; i < num ; i++) {
+		if (num % i === 0) {
+			divisible = true
+			break
+		}
 	}
 
-	let number = Math.round(Math.random() * (max - min) + min)
-
-	console.log(`El numero random es ${number}`);
+	return (divisible)
+		? console.log(`El número ${num}, No es primo`)
+		: console.log(`El número ${num}, Si es primo`)
 }
 
-console.groupCollapsed('Ejercicio 9')
-	numeroAleatorio()
-	numeroAleatorio('', 600)
-	numeroAleatorio('RDG', 600)
-	numeroAleatorio(501, 'fdsfsd')
-	numeroAleatorio(501, 600)
-console.groupEnd('Ejercicio 9')
+console.groupCollapsed('Ejercicio 12')
+	primo()
+	primo('320')
+	primo(true)
+	primo({})
+	primo(1)
+	primo(-13)
+	primo(13)
+	primo(200)
+console.groupEnd('Ejercicio 12')
 
 
 
 
 
-// 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+// 13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
 
-const capicua = (numero = 0) => {
-	if (!numero) return console.warn(`No ingresaste un numero`)
-	if (typeof numero !== 'number') return console.error(`El valor ${numero} no es un nuemro`)
+const parOImpar = (num = undefined) => {
+if (num === undefined) return console.warn(`No ingersaste un numero.`);
 
-	numero = numero.toString();
+if (typeof num !== 'number') return console.error(`El valor ${num} ingresado no es un numero.`);
 
-	let alRevez = numero.split('').reverse().join('')
+if (num === 0) return console.error(`La división entre cero no está definida.`);
 
-	return (numero === alRevez)
-		? console.info(`Si es capícua, Número original ${numero}, Número al revés ${alRevez}`)
-		: console.info(`No es capícua, Número original ${numero}, Número al revés ${alRevez}`)
+(num % 2 === 0 )
+	? console.info(`EL número ${num} es par.`)
+	: console.info(`EL número ${num} es impar.`)
 }
 
-console.groupCollapsed('Ejercicio 10')
-	capicua()
-	capicua('19')
-	capicua({})
-	capicua(18.99)
-	capicua(99.99)
-	capicua(2000)
-	capicua(2002)
-console.groupEnd('Ejercicio 10')
+console.groupCollapsed('Ejercicio 13')
+parOImpar()
+parOImpar('')
+parOImpar('FHFGJ')
+parOImpar(0)
+parOImpar(1)
+parOImpar(2)
+parOImpar(3)
+parOImpar(-4)
+parOImpar(5)
+console.groupEnd('Ejercicio 13')
 
 
 
 
 
-// 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+// 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
 
-const factorial = (num = undefined) => {
-	if (num === undefined) return console.warn('No ingresaste un numero')
-	if (typeof num !== 'number') return console.error(`El valor de ${num} ingresado, No es un numero`)
-	if (num === 0) return console.error('No se puede hacer un factorial de 0')
-	if (Math.sign(num) === -1) return console.error('No se puede hacer un factorial de un número negativo')
+const conversion = ( num = undefined, grado = undefined) =>{
+if (num === undefined) return console.warn('No ingresaste un valor a convertir')
 
-	let factorial = 1
+if (typeof num !== 'number') return console.error('El dato, No es un valor a convertir')
 
-	for (let i = 1; i < num + 1; i++) {
-		factorial *= i
-	}
 
-	console.log(factorial);
+if (grado === undefined) return console.warn('Ingresa un grado: F ó C')
+
+if (typeof grado !== 'string') return console.error('Este dato no es una medida de temperatura')
+
+if (grado.length !== 1 || !/(C|F)/i.test(grado) ) return console.error('Valor de la unidad no reconocida')
+
+
+if (/c/i.test(grado)) return console.info(`${num}C° = ${Math.round((num * ( 9 / 5)) + 32)}F°`);
+
+if (/f/i.test(grado)) return console.info(`${num}F° = ${Math.round(((num-32)*(5 / 9)))}C°`);
+
+
 }
 
-console.groupCollapsed('Ejercicio 11')
-	factorial()
-	factorial('5')
-	factorial([1, 2, 3])
-	factorial(0)
-	factorial(1)
-	factorial(-8)
-	factorial(5)
-console.groupEnd('Ejercicio 11')
+console.groupCollapsed('Ejercicio 14')
+
+conversion()
+conversion('2')
+conversion(2)
+conversion(2,true)
+conversion(2,'Hola')
+conversion(2,'E')
+conversion(0,'C')
+conversion(32,'f')
+
+console.groupEnd('Ejercicio 14')
