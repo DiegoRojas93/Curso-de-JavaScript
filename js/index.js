11e4dc224286678d7c509eb3a9ad689e768294c1
Diego Rojas <diegorojas431@gmail.com>
2021-09-20 20:28:45 -0500

    Ejercicio 8

--- js/index.js
+++ js/index.js
@@ -1,87 +1,102 @@
-/*------------------------- Ejercicio 7 ------------------------*/
+/*------------------------- Ejercicio 8 ------------------------*/
 
-// 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+// 24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
 
-const elevacionDeNumeros = (array = undefined) => {
-	if (array === undefined) return console.warn('No ingresaste un arreglo de números')
+const orden = (array = undefined) => {
+	if (array === undefined) return console.warn (`No ingresaste un array`)
 
-	if (!(array instanceof Array)) return console.error(`No ingresaste un Array`)
+	if (!(array instanceof Array)) return console.error (`No ingresaste un array de números`)
 
-	if(array.length === 0) return console.error(`El arreglo esta vacio`)
+	if (array.length === 0) return console.error (`El array esta vacio`)
 
-	for (let i = 0; i < array.length; i++) {
-		if (typeof array[i] !== 'number') return console.error(`El dato ${array[i]} de la posicion ${i+1} del array no es un número`)
+	for (const num of array) {
+		if(typeof num !== 'number') return console.error(`El array continene elementos que no son números`)
 	}
 
+	return console.info({
+		array,
+		asc: array.map(num => num).sort(),
+		desc: array.map(num => num).sort().reverse()
+	})
 
-	const array2 = array.map((num) => num**2)
-
-	console.log(array2);
 }
 
-console.groupCollapsed('Ejercicio 21')
-	elevacionDeNumeros()
-	elevacionDeNumeros(534)
-	elevacionDeNumeros('bsfkdjn')
-	elevacionDeNumeros({a: 1, b: 2, c: 3})
-	elevacionDeNumeros([1, 2, 'f'])
-	elevacionDeNumeros([1, 2, 3])
-	elevacionDeNumeros([1, 4, 5])
-console.groupEnd('Ejercicio 21')
+console.groupCollapsed('Ejercicio 24')
+	orden()
+	orden('dsg')
+	orden([])
+	orden([1, {a: 1}, 5])
+	orden([7, 5, 7, 8, 6])
+console.groupEnd('Ejercicio 24')
 
 
 
 
 
-// 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+// 25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
 
 const filtro = (array = undefined) => {
-	if (array === undefined) return console.warn('No ingresaste un arreglo de números')
+if (array === undefined) return console.warn (`No ingresaste un array`)
 
-	if (!(array instanceof Array)) return console.error(`No ingresaste un Array`)
+if (!(array instanceof Array)) return console.error (`No ingresaste un array de números`)
 
-	if(array.length === 0) return console.error(`El arreglo esta vacio`)
+if (array.length === 0) return console.error (`El array esta vacio`)
 
-	for (let i = 0; i < array.length; i++) {
-		if (typeof array[i] !== 'number') return console.error(`El dato ${array[i]} de la posicion ${i} del array no es un número`)
-	}
-
-	console.info(`Arreglo original ${array}\n Valor mayor: ${Math.max(...array)}\n Valor menor ${Math.min(...array)}`);
+for (const num of array) {
+	if(typeof num !== 'number') return console.error(`El array continene elementos que no son números`)
 }
 
-console.groupCollapsed('Ejercicio 22')
-	filtro()
-	filtro([1, 'g', 99, -60])
-	filtro([1, 4, 99, -60])
-	filtro([1, 4, 3, 25])
-console.groupEnd('Ejercicio 22')
+// return console.info({
+// 	original: array,
+// 	sinDuplicados: array.filter((value, index, self) => self.indexOf(value) === index)
+// })
 
-
-
-
-
-// 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
-
-const newArray = ( array = undefined) => {
-	if (array === undefined) return console.warn('No ingresaste un arreglo de números')
-
-	if (!(array instanceof Array)) return console.error(`No ingresaste un Array`)
-
-	if(array.length === 0) return console.error(`El arreglo esta vacio`)
-
-	for (let i = 0; i < array.length; i++) {
-		if (typeof array[i] !== 'number') return console.error(`El dato ${array[i]} de la posicion ${i} del array no es un número`)
-	}
-
-	return console.log({
-		pares: array.filter(num => num % 2 === 0),
-		impares: array.filter(num => num % 2 === 1)
-	});
+return console.info({
+	original: array,
+	sinDuplicados: [...new Set(array)]
+})
 }
 
-console.groupCollapsed('Ejercicio 23')
-	newArray()
-	newArray([1, 'g', 99, -60])
-	newArray([1, 4, 99, -60])
-	newArray([1, 2, 3, 4, 5])
-console.groupEnd('Ejercicio 23')
+console.groupCollapsed('Ejercicio 25')
+filtro()
+filtro('dsg')
+filtro([])
+filtro([1, {a: 1}, 5])
+filtro([7, 5, 7, 8, 6])
+console.groupEnd('Ejercicio 25')
+
+
+
+
+
+// 26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
+
+const promedio = (array = undefined) => {
+if (array === undefined) return console.warn (`No ingresaste un array`)
+
+if (!(array instanceof Array)) return console.error (`No ingresaste un array de números`)
+
+if (array.length === 0) return console.error (`El array esta vacio`)
+
+for (const num of array) {
+	if(typeof num !== 'number') return console.error(`El array continene elementos que no son números`)
+}
+
+return console.info(
+	array.reduce((total, num, index, array)=>{
+		total += num;
+		if(index === array.length - 1) {
+			return `El promedio de ${array.join(' + ')} es ${total/ array.length}`
+		}else{
+			return total;
+		}
+	})
+)
+}
+
+console.groupCollapsed('Ejercicio 26')
+promedio()
+promedio([])
+promedio([1, {a: 2}, 3])
+promedio([9,8,7,6,5,4,3,2,1,0])
+console.groupEnd('Ejercicio 26')
