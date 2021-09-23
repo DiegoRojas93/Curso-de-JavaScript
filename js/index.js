b5d79e4e061bc23518b13c12215537ba1e06277a
Diego Rojas <diegorojas431@gmail.com>
2021-09-22 21:29:56 -0500

    Propiedades Dinámicas de los Objetos

--- js/index.js
+++ js/index.js
@@ -1,50 +1,53 @@
 //---------------------Nuevos Datos en JavaScript-----------------
 
-// Proxies
+// Propiedades Dinámicas de los objetos
+
+let aleatorio = Math.round(Math.random() * 100 + 4)
+
+const objetoUsuarios = {
+	[`Id_${aleatorio}`] : 'Valor alatorio'
+};
+
+console.info(objetoUsuarios);
+
+const usuarios = ['Jesus', 'Diego', 'Maria', 'Jenaro']
 
 
-// Objeto literal
+usuarios.forEach((usuario, index) => objetoUsuarios[`Id_${index}`] = usuario );
 
-const persona = {
-	name: '',
-	lastName: '',
-	age: 0
-}
+console.info(objetoUsuarios);
 
-//Handler
 
-const manejador = {
-	set(obj, propiedades, valor){			// En el set creamos las funciones que validaran la creacion de la copia del objeto literal
 
-		if (Object.keys(obj).indexOf(propiedades) === -1){	// si la propiedad no exite en el objeto literal original retorname el error
+// Ejercicio
 
-			return console.error(`La propiedad "${propiedades}" no existe en el objeto persona`)
-		}
+class Persona {
+	constructor(nombre, apellido, edad){
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
 
-		if ((propiedades === 'name' || propiedades === 'lastName') && !(/^[A-Za-zÑñÁÉÍÓÚáéíóú\s]+$/g.test(valor))) {
-
-			// Si la pripiedad es name ó lastmane y el valor no cumple con la expresion regular, retorneme el error
-			return console.error(`La propiedad "${propiedades}" sólo acepta letras y espacio en blanco`)
-		}
-
-		obj[propiedades] = valor			// Creando la propiedad y el valor de la copia
+	saludar(){
+		console.info(`Hola, me llamo ${this.nombre} ${this.apellido} y tengo ${this.edad} años`)
 	}
 }
 
-// uso del PROXIE
+const JESUS = new Persona('Jesus', 'de Nazareth', 2020);
+const DIEGO = new Persona('Diego', 'Rojas', 27);
+const MARIA = new Persona('Maria', 'Quintero', 61);
 
-const DIEGO = new Proxy(persona, manejador)
+const LISTA = {}
+const PERSONAS = [JESUS, DIEGO, MARIA]
 
+console.info(PERSONAS);
+console.info('')
 
-console.info(persona)
-console.info(DIEGO)
-console.log('');
+PERSONAS.forEach((persona, index) => LISTA[`Id:${index}`] = persona);
 
-DIEGO.name = 'Diego';
-DIEGO.lastName = 'Rojas';
-DIEGO.age = 27;
-DIEGO.twitter = '@DiegoFrontend';  // arroja un error debido a que la propiedad no exite
+console.info(LISTA)
+console.info('')
 
-console.log('');
-console.info(persona)
-console.info(DIEGO)
+for (const key in LISTA) {
+	LISTA[key].saludar()
+}
