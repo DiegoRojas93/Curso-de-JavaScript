c66ade761b655563d970bddbaba1f5963ff9ad75
Diego Rojas <diegorojas431@gmail.com>
2021-09-27 19:57:56 -0500

    DOM: Introducción

--- js/index.js
+++ js/index.js
@@ -1,39 +1,26 @@
-//------------------JSON: JavaScript Object Notation--------------
+//-----------------------------WEB API´s-------------------------
 
-const DIEGO = {
-	nombre: 'Diego',
-	edad: 27,
-	booleano: true,
-	arreglo: ['Anime', 'Programar', 'Cine'],
-	objeto: {
-		twitter: '@DiegoFrontend',
-		email: '@gmail.com'
-	},
-	nulo: null
-}
+// El Dom
 
-// Parseo
+console.info(window.document)
+console.info(document)									// Nos trae el typo y todo el documento html
+console.info(document.doctype)					// Nos trae el typo de documento
+console.info(document.documentElement)	// Nos trae todo el documento
 
-console.info(JSON);
-console.info(JSON.parse("{}"));
-console.info(JSON.parse("[1, 2, 3]"));
-console.info(JSON.parse("true"));
-console.info(JSON.parse("false"));
-console.info(JSON.parse("19"));
-console.info(JSON.parse("null"));
-console.info(JSON.parse("Hola mundo!"));				// Una simple cadena no es un objeto valido para JSON
-console.info(JSON.parse("undefined"));					// undefinend no es valor aceptado para JSON
+console.info(document.head)							// Nos trae todo el head del html
+console.info(document.charset)					// Nos trae el typo de caracteres que trabacamos en el documento, osea en UTF-8
+console.info(document.styleSheets)			// accedemos a una coleccion de las hojas de stylos
+console.info(document.title)						// Nos trae el titulo del documento
 
+console.info(document.body)							// Nos trae todo el cuerpo del html
+console.info(document.scripts)					// accedemos a una coleccion de todos los scripts que usa el documento
 
-// Stringify
+console.info(document.links)						// Nos trae una lista (No son aregglos) de todos los links que tiene el documento, como ofertas y propagandas.
+console.info(document.images)						// Nos trae una lista de las imagenes que usa el documento
+console.info(document.forms)						// accedemos a una coleccion que nos devuelve el documento
 
-console.info(JSON);
-console.info(JSON.stringify({}));
-console.info(JSON.stringify([1, 2, 3]));
-console.info(JSON.stringify(true));
-console.info(JSON.stringify(false));
-console.info(JSON.stringify(19));
-console.info(JSON.stringify(null));
-console.info(JSON.stringify(undefined))
-console.info(JSON.stringify(DIEGO));
-console.info(JSON.stringify("Hola mundo!"));
+setTimeout(() => {
+	console.info(document.getSelection().toString())	// accedemos a una coleccion de toda la seleccion que hace el mause
+}, 3000);
+
+document.write(`<h1>Hola Mundo!</h1>`) // escribimos en el documento antes del cierre de la etiqueta body
