ebc0936792a2fb1859aa3080f9363de49ba907ee
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 19:42:40 -0500

    Modificando Elementos (nuevo estilo)

--- js/index.js
+++ js/index.js
@@ -1,25 +1,43 @@
 //-----------------------------DWEB API´s-------------------------
 
 const $cards = document.querySelector('.cards')
-			$newCard = document.createElement('figure')
+const $newCard = document.createElement('figure')
 
 $newCard.classList.add('card')
 
-$newCard.innerHTML = `
-	<img src="https://placeimg.com/200/200/any" alt="Any" />
-	<figcaption>Any</figcaption>
-`;
+// $newCard.innerHTML = `
+// 	<img src="https://placeimg.com/200/200/any" alt="Any" />
+// 	<figcaption>Any</figcaption>
+// `;
 
 
 
-// Modificando Elementos (viejo estilo)
+// Modificando Elementos (nuevo estilo)
 
-$cards.replaceChild($newCard, $cards.children[2]); // reemplazando un nodo hijo por otro
+    // $cards.insertAdjacentElement('beforebegin',$newCard)    // insertame el elemento antes del cards
 
-$cards.insertBefore($newCard, $cards.firstElementChild) // Agregame un nodo antes del el nodo que le indiquemos
+    // $cards.insertAdjacentElement('afterbegin',$newCard)  // insertame el elemento como primer hijo de cards
 
-$cards.removeChild($cards.lastElementChild) // remuevame el nodo hijo
+    // $cards.insertAdjacentElement('beforeend',$newCard)   // insetame el elemento como el ultimo hijo de cards
 
-const $clonCards = $cards.cloneNode(true);	// true = traeme su contenido
+    // $cards.insertAdjacentElement('afterend',$newCard)    // insetame el elemento despues de cards
 
-document.body.appendChild($clonCards)
+
+    let $contendCard = `
+        <img src="https://placeimg.com/200/200/any" alt="Any" />
+        <figcaption></figcaption>
+    `;
+
+    $newCard.insertAdjacentHTML('beforeend', $contendCard)
+
+    $newCard.querySelector('figcaption').insertAdjacentText('afterbegin', 'Any')
+
+    // $cards.insertAdjacentElement('afterbegin', $newCard)
+
+
+// Insertando el elementos con médodos usados en JQUERY
+
+    $cards.prepend($newCard)
+    // $cards.append($newCard)
+    // $cards.after($newCard)
+    // $cards.before($newCard)
