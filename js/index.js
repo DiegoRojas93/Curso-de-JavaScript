af15725c153ca35ade2de1684584265c3b2021e7
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 20:51:35 -0500

    DOM: Ejercicios Prácticos | Responsive Tester

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 import scrollTopButton from "./modules/boton_scroll.js";
 import countDown from "./modules/countdown.js";
 import hamburguerMenu from "./modules/menu_ambuguesa.js";
+import responsiveTester from "./modules/prueba_responsive.js";
 import { alarm, digitalClock } from "./modules/reloj_alarma.js";
 import responsiveMedia from "./modules/responsive_design.js";
 import { moveBall, shortcuts } from "./modules/teclado.js";
@@ -14,6 +15,11 @@ const d = document;
 
 
 d.addEventListener('DOMContentLoaded', e => {
+	hamburguerMenu('.panel-btn', '.panel', '.menu a');
+	digitalClock('#hora', '#iniciar_reloj', '#detener_reloj')
+	alarm('assets/audio/alarmaDeGuerra.mp3', '#iniciar_alarma', '#detener_alarma')
+	countDown('countdown', 'Sep 11, 2021 01:39:00', 'Feliz cumpleaños Diego')
+	scrollTopButton('.scroll-top-btn')
 	responsiveMedia(
 		'youtube',
 		"(min-width : 1024px)",
@@ -27,16 +33,9 @@ d.addEventListener('DOMContentLoaded', e => {
 		`<a href="https://goo.gl/maps/rNLLRvZAkqJNhbjR8" target="_blank" real="noopener">Monserrate</a>`,
 		`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15907.739245041013!2d-74.06428027408177!3d4.605694084955603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f99bfd2a459a9%3A0x352a5134b65e607b!2sMonserrate!5e0!3m2!1ses!2sco!4v1612390647569!5m2!1ses!2sco" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`
 	);
+
+	responsiveTester('responsive-tester')
 })
-
-	hamburguerMenu('.panel-btn', '.panel', '.menu a');
-	digitalClock('#hora', '#iniciar_reloj', '#detener_reloj')
-	alarm('assets/audio/alarmaDeGuerra.mp3', '#iniciar_alarma', '#detener_alarma')
-	countDown('countdown', 'Sep 11, 2021 01:39:00', 'Feliz cumpleaños Diego')
-	scrollTopButton('.scroll-top-btn')
-
-
-
 
 d.addEventListener('keydown', e =>{
 	shortcuts(e);
