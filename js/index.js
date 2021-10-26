0c751d644fca6ec43eb3c926a710a7973248854f
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 19:53:17 -0500

    BOM: Propiedades y Eventos

--- js/index.js
+++ js/index.js
@@ -1,26 +1,45 @@
 //-----------------------------DWEB API´s-------------------------
 
+// BOM: Propiedades y Eventos
 
-// DOM: Delegación de Eventos
+window.addEventListener('resize', e => { // resize se activa cuando se cambia el tamaño de la vista del documento
+
+  console.clear()
+  console.log(`**************** Evento Resize *****************`);
+  console.log(window.innerWidth);   // ancho interior
+  console.log(window.innerHeight);  // alto interior
+  console.log(window.outerWidth);   // ancho exterior
+  console.log(window.outerHeight);  // alto exterior
+  console.log(e);
+})
 
 
-function flujoEventos(e) {
-	console.log(`Hola te saluda el div ${this}, el click lo origino el div ${e.target.className}`);
-}
+
+window.addEventListener('scroll', e =>{ // scroll se activa cuando movemos el scroll de la vantana
+
+  console.clear();
+  console.log(`**************** Evento Scroll *****************`);
+  console.log(window.scrollX);      // Barra de desplazamiento X
+  console.log(window.scrollY);      // Barra de desplazamiento Y
+  console.log(e);
+})
 
 
 
-document.addEventListener('click', (e)=>{
-	console.log(`Click en: `, e.target);
+window.addEventListener('load', e => { // load se activa cuando el documento termina de cargar
 
-	// matches() comprueba si el Element sería seleccionable por el selector CSS
+  console.log(`***************** Evento Load ******************`);
+  console.log(window.screenX);       // coordenada en X el que se dibuja el browser un al pantalla del computador
+  console.log(window.screenY);       // coordenada en Y el que se dibuja el browser un al pantalla del computador
+  console.log(e);
+})
 
-	if (e.target.matches('.eventos-flujo div')) {
-		flujoEventos(e)
-	}
 
-	if (e.target.matches('.eventos-flujo a')) {
-		alert(`Hola soy Diego Rojas`)
-		e.preventDefault()
-	}
+
+document.addEventListener('DOMContentLoaded', e => { // DOMContendLoaded se activa cuando el documento empieza a cargar
+
+  console.log(`*********** Evento DOMContentLoaded ************`);
+  console.log(window.screenX);       // cordena en X el que se dibuja el navegador un al patalla del computador
+  console.log(window.screenY);       // cordena en Y el que se dibuja el navegador un al patalla del computador
+  console.log(e);
 })
