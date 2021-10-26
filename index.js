edeba6e5e43ee2029e6a53ba3fbd48433084fe1c
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 21:47:14 -0500

    API REST: CRUD

---
+++ index.js
@@ -0,0 +1,6 @@
+const d = document,
+			$table = d.querySelector('.crud-table'),
+			$form = d.querySelectorAll('crud-form'),
+			$title = d.querySelectorAll('crud-title'),
+			$templete = d.querySelectorAll('crud-templete').content,
+			$fragment = d.createDocumentFragment();
