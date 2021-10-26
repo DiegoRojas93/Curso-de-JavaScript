67e45358185d9ca620a6ecf61946d31760a611bf
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 21:39:23 -0500

    AJAX: API Fetch

--- index.js
+++ index.js
@@ -35,8 +35,39 @@
 
 
 
-	xhr.open('GET', 'https://jsonplaceholder.typicode.com/user')
+	xhr.open('GET', 'https://jsonplaceholder.typicode.com/users')
 
 	xhr.send()
 
 })(document, window, console);
+
+
+((d, w, c)=>{
+	const $fetch = d.getElementById('fetch'),
+				$fragment = d.createDocumentFragment();
+
+	fetch('https://jsonplaceholder.typicode.com/users')
+	// El then y el catch resultan ser las validadiciones de código de respuesta
+		.then(res => res.ok ? res.json() : Promise.reject(res))
+		.then(json => {
+
+			json.forEach(el => {
+				const $li = d.createElement('li')
+
+				$li.innerHTML = `${el.name} --- ${el.email} --- ${el.phone}`
+
+				$fragment.appendChild($li)
+			})
+
+			$fetch.appendChild($fragment)
+		})
+		.catch(err => {
+			let message = err.statusText || 'Ocurrio un Error';
+
+			$fetch.innerHTML = `Error ${err.status}: ${message}`;
+		})
+		.finally(() => c.log('Este código se ejecutará independientemente del resultado de la promesa Fetch'))
+
+
+})(document, window, console);
+
