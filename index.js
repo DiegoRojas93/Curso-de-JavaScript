a3929d851718a29188ed45e3baff790e45fbee86
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 21:51:29 -0500

    API REST: CRUD con Fetch (2/2)

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const getAll = async() => {
 		let res = await fetch(`http://localhost:3001/santos`),
 				json = await res.json();
 
+
 		if (!res.ok) throw {status: res.status, statusText: res.statusText}
 
 		json.forEach(el => {
 			$template.querySelector('.name').textContent = el.nombre
 			$template.querySelector('.constellation').textContent = el.constelacion
 			$template.querySelector('.edit').dataset.id = el.id
+			$template.querySelector('.edit').dataset.name = el.nombre
 			$template.querySelector('.edit').dataset.constellation = el.constelacion
 			$template.querySelector('.delete').dataset.id = el.id
 
@@ -37,3 +39,100 @@ const getAll = async() => {
 
 
 d.addEventListener('DOMContentLoaded', getAll)
+
+
+d.addEventListener('submit', async e => {
+	if(e.target === $form){
+		e.preventDefault();
+
+		if(!e.target.id.value){
+			// POST
+
+			try {
+				let options = {
+					method: 'POST',																									// ingresamos el método
+					headers:{"Content-Type" : "application/json; charset=utf-8"},		// ingremos la cabecera
+					body: JSON.stringify({																					// ingresamos los datos que parsearemos a string
+						nombre: e.target.nombre.value,
+						constelacion: e.target.constelacion.value
+					})
+				};
+
+				let res = await fetch(`http://localhost:3001/santos`, options);
+
+				if(!res.ok) throw { status: res.status, statusText: res.statusText}
+
+				location.reload()
+
+			} catch (error) {
+
+				let message = error.statusText || 'Not Found';
+				$form.insertAdjacentHTML('afterend', `<p><b>Error ${error.status}: ${message} </b></p>`)
+			}
+
+		}else{
+			// PUT
+
+			try {
+				let options = {
+					method: 'PUT',
+					headers: {"Content-Type" :  "application/json; charset=utf-8"},
+					body: JSON.stringify({
+						nombre: e.target.nombre.value,
+						constelacion: e.target.constelacion.value
+					})
+				},
+					res = await fetch(`http://localhost:3001/santos/${e.target.id.value}`, options);
+
+				if(!res.ok) throw {status: res.status, statusText: res.statusText}
+
+				location.reload()
+
+			} catch (error) {
+				let message = error.statusText || `No se pudo editar`;
+
+				$form.insertAdjacentHTML('afterend', `<p><b>Error: ${error.status}: ${message}</b></p>`)
+			}
+		}
+	}
+})
+
+d.addEventListener('click', async e =>{
+
+	if (e.target.matches('.edit')) {
+
+		$title.textContent = `Editar Santo`;
+
+		$form.nombre.value = e.target.dataset.name;
+
+		$form.constelacion.value = e.target.dataset.constellation;
+
+		$form.id.value = e.target.dataset.id;
+	}
+
+
+
+	if (e.target.matches('.delete')){
+		let confirmacion = confirm(`¿Estas seguro de eliminar el id ${e.target.dataset.id}?`)
+
+		if(confirmacion) {
+			try {
+
+				let options = {
+					method: 'DELETE',
+					headers: {"Content-Type": "application/json; charset=utf-8"}
+				},
+					res = await fetch(`http://localhost:3001/santos/${e.target.dataset.id}`, options);
+
+				if (!res.ok) throw {status: res.status, statusText: res.statusText};
+
+				location.reload()
+
+			} catch (error) {
+				let message = error.statusText || `No se pudo eliminar`;
+
+				alert(`Error: ${error.status}: ${message}`)
+			}
+		}
+	}
+});
