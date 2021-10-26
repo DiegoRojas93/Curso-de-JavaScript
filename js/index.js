b3e3c67a6fa49a7d05c64fc641d33a0ac6e54373
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 20:38:58 -0500

    DOM: Ejercicios | Eventos del Teclado (movimientos y colisiones)

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 import hamburguerMenu from "./modules/menu_ambuguesa.js";
 import { alarm, digitalClock } from "./modules/reloj_alarma.js";
-import { shortcuts } from "./modules/teclado.js";
+import { moveBall, shortcuts } from "./modules/teclado.js";
 
 const d = document;
 
@@ -10,8 +10,12 @@ d.addEventListener('DOMContentLoaded', e => {
 	hamburguerMenu('.panel-btn', '.panel', '.menu a');
 	digitalClock('#hora', '#iniciar_reloj', '#detener_reloj')
 	alarm('assets/audio/alarmaDeGuerra.mp3', '#iniciar_alarma', '#detener_alarma')
+	moveBall
 })
 
 
 
-d.addEventListener('keydown', e => shortcuts(e))
+d.addEventListener('keydown', e =>{
+	shortcuts(e);
+	moveBall(e, '.ball', '.stage')
+})
