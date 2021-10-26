fcd05ac73f9c045cb7d5f136edce534420cfa454
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 19:34:29 -0500

    DOM: Texto y HTML

--- js/index.js
+++ js/index.js
@@ -1,62 +1,24 @@
 //-----------------------------DWEB API´s-------------------------
 
-// DOM: Clases CSS
+// DOM: DOM: Texto y HTML
 
-const $card = document.querySelectorAll('.card');
+const $parrafoDOM = document.getElementById('que-es')
 
-// Conociendo los nombres de las clases
+let text = `
+	<p>
+		El modelo de Objetos del Domcumento (<b><i>DOM => document Object Model</i></b>) es un API para documentos HTML y XML
+	</p>
+	<P>
+		Éste proveé una representacion estructural del documento, permitiendo modificar su contenido y representacion visual mediate el código JS.
+	</P>
+	<P>
+		<mark>
+			El DOM no es parte de la especificación de JavaScript , es na API para los navegadores
+		</mark>
+	</P>
+`;
 
-	console.log($card[0].className); // trae el en una cadena de texto el valor del atributo class del elemento
-
-	console.log($card[1].className);
-
-
-	console.log($card[0].classList); // trae el objeto DOMtokenList, el cual es una lista que muestra el nombre y el valor del tributo class
-	console.log($card[1].classList); // trae el objeto DOMtokenList, el cual es una lista que muestra el nombre y el valor del tributo class
-
-
-
-// evaluar si un elemento contiene una clase en particular
-
-	console.log($card[1].classList.contains('card'));
-	console.log($card[1].classList.contains('card-2'));
-	console.log($card[1].classList.contains('card card-2'));
-
-
-
-// Anadiendo el nombre de una clase
-
-	console.log($card[0].classList.contains('rotate-45'));
-
-	$card[0].classList.add('rotate-45')
-
-	console.log($card[0].classList.contains('rotate-45'))
-
-
-
-// Anadiendo el nombre de una clase
-
-	$card[0].classList.remove('rotate-45')
-
-	console.log($card[0].classList.contains('rotate-45'))
-
-
-
-// usando el método .toogle()
-
-	$card[1].classList.toggle('rotate-45')	// ejecuta el método .add()
-	console.log($card[1].classList.contains('rotate-45'));
-
-	$card[1].classList.toggle('rotate-45')	// ejecuta el método .remove()
-	console.log($card[1].classList.contains('rotate-45'));
-
-
-
-// remplazando el nombre de una clase
-
-	$card[1].classList.add('rotate-45')
-
-	setTimeout(() => {
-		$card[1].classList.replace('rotate-45', 'rotate-135')
-	}, 10000);
-
+$parrafoDOM.innerText = text // contiene identacion y no diferencia etiquetas HTML
+$parrafoDOM.textContent = text	// NO contiene identacion y no diferencia etiquetas HTML
+$parrafoDOM.innerHTML = text	// renderiza el texto identificando las etiquetas HTML
+$parrafoDOM.outerHTML = text	// evitar crear más elementos dentro en la etiqueta p
