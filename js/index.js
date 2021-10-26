c825be6561bd21309b88b6d2e3ba6bf584fd9146
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 19:55:09 -0500

    BOM: Métodos

--- js/index.js
+++ js/index.js
@@ -1,45 +1,27 @@
-//-----------------------------DWEB API´s-------------------------
+//-----------------------------WEB API´s-------------------------
 
-// BOM: Propiedades y Eventos
+// BOM: Métodos
 
-window.addEventListener('resize', e => { // resize se activa cuando se cambia el tamaño de la vista del documento
+// window.alert('Alerta')
+// window.confirm('Comfimacion')
+// window.prompt('¿Que quieres hacer?')
 
-  console.clear()
-  console.log(`**************** Evento Resize *****************`);
-  console.log(window.innerWidth);   // ancho interior
-  console.log(window.innerHeight);  // alto interior
-  console.log(window.outerWidth);   // ancho exterior
-  console.log(window.outerHeight);  // alto exterior
-  console.log(e);
-})
+const $btnAbrir = document.getElementById('abrir-ventana'),
+      $btnCerrar = document.getElementById('cerrar-ventana'),
+      $btnImprimir = document.getElementById('imprimir-ventana');
 
-
-
-window.addEventListener('scroll', e =>{ // scroll se activa cuando movemos el scroll de la vantana
-
-  console.clear();
-  console.log(`**************** Evento Scroll *****************`);
-  console.log(window.scrollX);      // Barra de desplazamiento X
-  console.log(window.scrollY);      // Barra de desplazamiento Y
-  console.log(e);
-})
+let vantana;
 
 
 
-window.addEventListener('load', e => { // load se activa cuando el documento termina de cargar
+$btnAbrir.addEventListener('click', e =>{
+  ventana = window.open('https://aprendejavascript.org/') // abre una nueva ventana especificando la ulr
+});
 
-  console.log(`***************** Evento Load ******************`);
-  console.log(window.screenX);       // coordenada en X el que se dibuja el browser un al pantalla del computador
-  console.log(window.screenY);       // coordenada en Y el que se dibuja el browser un al pantalla del computador
-  console.log(e);
-})
+$btnCerrar.addEventListener('click', e =>{
+  ventana.close() // cierra la ventana en la que se hace referencia a la variable
+});
 
-
-
-document.addEventListener('DOMContentLoaded', e => { // DOMContendLoaded se activa cuando el documento empieza a cargar
-
-  console.log(`*********** Evento DOMContentLoaded ************`);
-  console.log(window.screenX);       // cordena en X el que se dibuja el navegador un al patalla del computador
-  console.log(window.screenY);       // cordena en Y el que se dibuja el navegador un al patalla del computador
-  console.log(e);
-})
+$btnImprimir.addEventListener('click', e =>{
+  window.print() // Abre el Diálogo de Impresión para imprimir la vista del documento actual.
+});
