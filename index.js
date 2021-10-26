ff464ac85dd725ff07a6a933cf5d5aacdbde72bd
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 21:54:27 -0500

    Ejercicios AJAX - APIs: Sitio web con Ajax

--- index.js
+++ index.js
@@ -1,125 +1,44 @@
 const d = document,
-			$table = d.querySelector('.crud-table'),
-			$form = d.querySelector('.crud-form'),
-			$title = d.querySelector('#crud-title'),
-			$template = d.querySelector('#crud-template').content,
-			$fragment = d.createDocumentFragment();
+      $main = d.querySelector('main');
 
+const getHTML = (options) => {
+  let { url, success, error } = options;
+  const xhr = new XMLHttpRequest();
 
+  xhr.addEventListener('readystatechange', e => {
+    if (xhr.readyState !== 4) return;
 
-const getAll = async() => {
-	try {
-		let res = await axios.get(`http://localhost:3001/santos`),
-				json = await res.data;
+    if (xhr.status >= 200 && xhr.status < 300) {
+      let html = xhr.responseText;      // No vamos a parsear código JSON sino a HTML
 
-		json.forEach(el => {
-			$template.querySelector('.name').textContent = el.nombre;
-			$template.querySelector('.constellation').textContent = el.constelacion;
+      success(html)
 
-			$template.querySelector('.edit').dataset.id = el.id
-			$template.querySelector('.edit').dataset.name = el.nombre
-			$template.querySelector('.edit').dataset.constellation = el.constelacion
-			$template.querySelector('.delete').dataset.id = el.id
+    } else {
+      let message = xhr.statusText || `Ocurrió un error`;
+      error(`Error ${xhr.status}: ${message}`);
+    }
+  });
 
-			let $clone = $template.cloneNode(true)
+  xhr.open('GET', url);
+  xhr.setRequestHeader("Content-type", "text/html; charset=utf-8");
+  xhr.send();
+};
 
-			$fragment.appendChild($clone)
-		});
-
-		$table.querySelector('tbody').appendChild($fragment)
-
-	} catch (err) {
-		let message = err.request.statusText || 'Not Found'
-
-		$table.insertAdjacentHTML('afterend', `<p><b>Error ${err.request.status}: ${message}</b></p>`)
-	}
-}
-
-
-d.addEventListener('DOMContentLoaded', getAll());
-
-
-d.addEventListener('submit', async e => {
-	e.preventDefault();
-
-	if(e.target ===  $form){
-
-		if (!e.target.id.value) {
-			// POST
-
-			try {
-				let options = {
-					method: 'POST',
-					headers: {'Content-Type': 'application/json; charset=utf-8'},
-					data: JSON.stringify({
-						nombre: e.target.nombre.value,
-						constelacion: e.target.constelacion.value
-					})
-				},
-					res = await axios('http://localhost:3001/santos', options);
-
-				location.reload()
-
-			} catch (error) {
-				let message = error.request.statusText
-
-				$form.insertAdjacentHTML('afterend',`<p><b>Error ${error.request.status}: ${message}</b></p>`)
-			}
-
-		}else{
-			// PUT
-
-			try {
-				let res = await axios({
-					method: 'PUT',
-					url: `http://localhost:3001/santos/${e.target.id.value}`,
-					headers: {'Content-Type': 'application/json; charset=utf-8'},
-					data: {
-						nombre: e.target.nombre.value,
-						constelacion: e.target.constelacion.value
-					}
-				});
-
-				location.reload()
-
-			} catch (error) {
-				let message = error.request.statusText
-
-				$form.insertAdjacentHTML('afterend',`<p><b>Error ${error.request.status}: ${message}</b></p>`)
-			}
-		}
-
-	}
-
+d.addEventListener('DOMContentLoaded', (e) => {
+  getHTML({
+    url: "assets/home.html",
+    success:(html) => $main.innerHTML = html,
+    error:(err) => $main.innerHTML = `<h1>${err}</h1>`
+  });
 });
 
-d.addEventListener('click', async e => {
-	if (e.target.matches('.edit')) {
-		$title.innerHTML = 'Editar Santo'
-
-		$form.nombre.value = e.target.dataset.name
-		$form.constelacion.value = e.target.dataset.constellation
-		$form.id.value = e.target.dataset.id
-	}
-
-	if (e.target.matches('.delete')) {
-		// DELETE
-
-		let confirmacion = confirm(`Estas seguro de eliminar el id ${e.target.dataset.id}`)
-
-		if (confirmacion){
-			try {
-				let res = await axios({
-					method: 'DELETE',
-					url: `http://localhost:3001/santos/${e.target.dataset.id}`,
-					headers: {'Content-Type': 'application/json; charset=utf-8'}
-				})
-
-			} catch (error) {
-				let message = error.request.statusText
-
-				alert(`Error ${error.request.status}: ${message}`)
-			}
-		}
-	}
+d.addEventListener('click', e => {
+  if (e.target.matches(".menu a")) {
+    e.preventDefault();
+    getHTML({
+      url: e.target.href,
+      success:(html) => $main.innerHTML = html,
+      error:(err) => $main.innerHTML = `<h1>${err}</h1>`
+    });
+  }
 });
