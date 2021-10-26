f2adcccc44a26302cde267a2cfb10c8b1436eb8a
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 21:53:22 -0500

    API REST: CRUD con Axios: (2/2)

--- index.js
+++ index.js
@@ -37,3 +37,89 @@ const getAll = async() => {
 
 
 d.addEventListener('DOMContentLoaded', getAll());
+
+
+d.addEventListener('submit', async e => {
+	e.preventDefault();
+
+	if(e.target ===  $form){
+
+		if (!e.target.id.value) {
+			// POST
+
+			try {
+				let options = {
+					method: 'POST',
+					headers: {'Content-Type': 'application/json; charset=utf-8'},
+					data: JSON.stringify({
+						nombre: e.target.nombre.value,
+						constelacion: e.target.constelacion.value
+					})
+				},
+					res = await axios('http://localhost:3001/santos', options);
+
+				location.reload()
+
+			} catch (error) {
+				let message = error.request.statusText
+
+				$form.insertAdjacentHTML('afterend',`<p><b>Error ${error.request.status}: ${message}</b></p>`)
+			}
+
+		}else{
+			// PUT
+
+			try {
+				let res = await axios({
+					method: 'PUT',
+					url: `http://localhost:3001/santos/${e.target.id.value}`,
+					headers: {'Content-Type': 'application/json; charset=utf-8'},
+					data: {
+						nombre: e.target.nombre.value,
+						constelacion: e.target.constelacion.value
+					}
+				});
+
+				location.reload()
+
+			} catch (error) {
+				let message = error.request.statusText
+
+				$form.insertAdjacentHTML('afterend',`<p><b>Error ${error.request.status}: ${message}</b></p>`)
+			}
+		}
+
+	}
+
+});
+
+d.addEventListener('click', async e => {
+	if (e.target.matches('.edit')) {
+		$title.innerHTML = 'Editar Santo'
+
+		$form.nombre.value = e.target.dataset.name
+		$form.constelacion.value = e.target.dataset.constellation
+		$form.id.value = e.target.dataset.id
+	}
+
+	if (e.target.matches('.delete')) {
+		// DELETE
+
+		let confirmacion = confirm(`Estas seguro de eliminar el id ${e.target.dataset.id}`)
+
+		if (confirmacion){
+			try {
+				let res = await axios({
+					method: 'DELETE',
+					url: `http://localhost:3001/santos/${e.target.dataset.id}`,
+					headers: {'Content-Type': 'application/json; charset=utf-8'}
+				})
+
+			} catch (error) {
+				let message = error.request.statusText
+
+				alert(`Error ${error.request.status}: ${message}`)
+			}
+		}
+	}
+});
