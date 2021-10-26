f952d049832c224e8ee9280bc150737addea7c16
Diego Rojas <diegorojas431@gmail.com>
2021-10-25 19:40:34 -0500

    Modificando Elementos (viejo estilo)

--- js/index.js
+++ js/index.js
@@ -1,41 +1,25 @@
 //-----------------------------DWEB API´s-------------------------
 
-// Templates HTML
-
 const $cards = document.querySelector('.cards')
-			$template = document.getElementById('template-card').content	// traeme solamente el contendido de la etiqueta
-			$fragment = document.createDocumentFragment()									// creame una fragmento de nodo
-			cardContent = [
-				{
-					title: 'Tecnología',
-					img:'https://placeimg.com/200/200/tech'
-				},
-				{
-					title: 'Animales',
-					img:'https://placeimg.com/200/200/animals'
-				},
-				{
-					title: 'Arquitectura',
-					img:'https://placeimg.com/200/200/arch'
-				},
-				{
-					title: 'Gente',
-					img:'https://placeimg.com/200/200/people'
-				},
-				{
-					title: 'Naturaleza',
-					img:'https://placeimg.com/200/200/nature'
-				}
-			]
+			$newCard = document.createElement('figure')
+
+$newCard.classList.add('card')
+
+$newCard.innerHTML = `
+	<img src="https://placeimg.com/200/200/any" alt="Any" />
+	<figcaption>Any</figcaption>
+`;
 
 
-cardContent.forEach(el => {
-	$template.querySelector('img').setAttribute('src', el.img)
-	$template.querySelector('img').setAttribute('alt', el.title)
-	$template.querySelector('figcaption').innerHTML = el.title
 
-	let $clone = document.importNode($template, true)					// cloname todo el contenido del template
-	$fragment.appendChild($clone)															// añade el contenido del clon al fragmento
-})
+// Modificando Elementos (viejo estilo)
 
-$cards.appendChild($fragment)																// Añade al elemento cards el fragmento de código
+$cards.replaceChild($newCard, $cards.children[2]); // reemplazando un nodo hijo por otro
+
+$cards.insertBefore($newCard, $cards.firstElementChild) // Agregame un nodo antes del el nodo que le indiquemos
+
+$cards.removeChild($cards.lastElementChild) // remuevame el nodo hijo
+
+const $clonCards = $cards.cloneNode(true);	// true = traeme su contenido
+
+document.body.appendChild($clonCards)
